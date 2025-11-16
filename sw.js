240b106b14b4a6ca10bfdbdbb71514980b761669
v18641864-del <v18641864@gmail.com>
2025-11-17 03:23:11 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,13 @@
-const CACHE_NAME = 'sublimation-calc-v1';
-const urlsToCache = [
-  '.',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("zt-art-cache").then((cache) => {
+      return cache.addAll(["./", "./index.html", "./icon.png"]);
+    })
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(k => { if (k !== CACHE_NAME) return caches.delete(k); })
-    ))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((resp) => resp || fetch(e.request))
   );
 });
