d1f7ce957ea98430d69cf09b819b842bc9cbe335
v18641864-del <v18641864@gmail.com>
2025-11-17 01:48:37 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'sublimation-calc-v1';
+const urlsToCache = [
+  '.',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(k => { if (k !== CACHE_NAME) return caches.delete(k); })
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
