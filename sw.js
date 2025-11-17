e60fd1d43b9112ec2222c5d543f905450e19cd1c
v18641864-del <v18641864@gmail.com>
2025-11-17 20:48:14 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,26 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("zt-art-cache").then((cache) => {
-      return cache.addAll(["./", "./index.html", "./icon.png"]);
-    })
+// простой service worker - кеширование
+const CACHE = 'ztart-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((resp) => resp || fetch(e.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(r => r || fetch(event.request))
   );
 });
